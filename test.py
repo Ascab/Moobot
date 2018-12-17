c77d2d2409596b3bf3718d2e497214c8cdb2fdae
Ascab <gamest0rm78@gmail.com>
2018-12-17 13:27:48 +0100

    working on purge

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import discord
 from discord.ext import commands
 
 TOKEN = 'NTIzOTk5MzY1Mzc5OTE1ODE2.DvhsKg.LwPsafPJ1gqL5gfJqjzAZLCG1Nw'
-
+idpatarotage='374187578846347264'
 description = '''Bot Python'''
 bot = commands.Bot(command_prefix='?', description=description)
-
+server = discord.Client.get_server(idpatarotage)
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -20,7 +20,19 @@ async def hello():
 @bot.command()
 async def tg(usr: discord.Member ):
 	await bot.say('TG' + usr.mention )	
-bot.run(TOKEN)
+
 @bot.command()
-async def purge(usr: discord.Member):
-	await purge_from(
+async def dududu():
+	await bot.say("https://www.youtube.com/watch?v=y6120QOlsfU")
+	
+def is_me(m):
+    return m.author == bot.user
+def is_he(m, usr: discord.Member):
+	return m.author == usr 
+@bot.command()
+async def purge(usr=None):
+	channel=discord.utils.get(server.channels, name='bip-boup-bip', type=ChannelType.text)	
+	deleted = await bot.purge_from(channel, limit=100, check=is_he(usr))
+	await bot.send_message(m.channel, 'Deleted {} message(s)'.format(len(deleted)))
+
+bot.run(TOKEN)
