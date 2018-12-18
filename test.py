b95f16d0a3700b054aa8cda04eba992fbac95fec
Ascab <gamest0rm78@gmail.com>
2018-12-18 23:12:51 +0100

    purge command

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import os
+import time
+from datetime import datetime
 
 os.chdir("/home/ascab/Documents/BotSondage")
 with open('token.txt', 'r') as tokenfile:
@@ -10,17 +12,14 @@ with open('token.txt', 'r') as tokenfile:
 idpatarotage="374187578846347264"
 description = '''Bot Python'''
 bot = commands.Bot(command_prefix='?', description=description)
-Client = Bot('!')
-#~ serv = serv.get_server(idpatarotage)
 @bot.event
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)	
     print(bot.user.id)
     print (discord.__version__)
-    #~ print(serv.servers)
-    print(type(bot))
     print('------')
+    
 
 @bot.command()
 async def hello():
@@ -38,13 +37,33 @@ def is_me(m):
     return m.author == bot.user
 def is_he(m, usr: discord.Member):
 	return m.author == usr 
-@bot.command()
-async def purge(n: int):
-	channel=discord.utils.get(bot.channels, name='bip-boup-bip', type=ChannelType.text)	
-	deleted = await Client.purge_from(channel, limit=100, check=is_he(usr))
-	await bot.send_message(m.channel, 'Deleted {} message(s)'.format(len(deleted)))
+@bot.command(pass_context=True)
+async def purge(ctx, n: int, usr=None):
+	channel=ctx.message.channel
+	timestamp = time.time()
+	utctime = datetime.utcfromtimestamp(timestamp-(12*3600))
+	msg = []
+	if usr!=None :
+		target = ctx.message.mentions.pop().id
+		async for x in bot.logs_from(ctx.message.channel, limit = int(n), before=ctx.message ,after=utctime, around=None) : 
+			if x.author.id == target : 
+				msg.append(x)
+	else :
+		async for x in bot.logs_from(channel, limit=int(n), before=ctx.message ,after=utctime, around=None) :
+		 	msg.append(x)	 	
+	await bot.delete_messages(msg)
+	await bot.say('Deleted {} message(s)'.format(len(msg)))
 @bot.command()
 async def exit():
 	bot.close()
-	
+@bot.command(pass_context = True)
+async def clear(ctx, number):
+    mgs = [] #Empty list to put all the messages in the log
+    number = int(number) #Converting the amount of messages to delete to an integer
+    async for x in bot.logs_from(ctx.message.channel, limit = number):
+        mgs.append(x)
+    await bot.delete_messages(mgs)
+@bot.command(pass_context=True)
+async def robin(ctx) :
+	await bot.send_message(discord.Object(id='374187578846347266'), 'Bon anniversaire'+ ctx.message.server.get_member('184685136372039680').mention + 'fait peter le champagne !')
 bot.run(TOKEN)
