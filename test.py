5b30cacc425aaf5d7d83ef04983b861eab00fc3e
Ascab <gamest0rm78@gmail.com>
2018-12-18 18:04:15 +0100

    Added .gitignore

--- test.py
+++ test.py
@@ -1,16 +1,25 @@
 import discord
 from discord.ext import commands
+import os
 
-TOKEN = 'NTIzOTk5MzY1Mzc5OTE1ODE2.DvhsKg.LwPsafPJ1gqL5gfJqjzAZLCG1Nw'
-idpatarotage='374187578846347264'
+os.chdir("/home/ascab/Documents/BotSondage")
+with open('token.txt', 'r') as tokenfile:
+	TOKEN = (tokenfile.read()).strip("\n")
+	print(TOKEN)
+	
+idpatarotage="374187578846347264"
 description = '''Bot Python'''
 bot = commands.Bot(command_prefix='?', description=description)
-server = discord.Client.get_server(idpatarotage)
+Client = Bot('!')
+#~ serv = serv.get_server(idpatarotage)
 @bot.event
 async def on_ready():
     print('Logged in as')
-    print(bot.user.name)
+    print(bot.user.name)	
     print(bot.user.id)
+    print (discord.__version__)
+    #~ print(serv.servers)
+    print(type(bot))
     print('------')
 
 @bot.command()
@@ -30,9 +39,12 @@ def is_me(m):
 def is_he(m, usr: discord.Member):
 	return m.author == usr 
 @bot.command()
-async def purge(usr=None):
-	channel=discord.utils.get(server.channels, name='bip-boup-bip', type=ChannelType.text)	
-	deleted = await bot.purge_from(channel, limit=100, check=is_he(usr))
+async def purge(n: int):
+	channel=discord.utils.get(bot.channels, name='bip-boup-bip', type=ChannelType.text)	
+	deleted = await Client.purge_from(channel, limit=100, check=is_he(usr))
 	await bot.send_message(m.channel, 'Deleted {} message(s)'.format(len(deleted)))
-
+@bot.command()
+async def exit():
+	bot.close()
+	
 bot.run(TOKEN)
